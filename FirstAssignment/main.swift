67089a34c95f8c62e19fc3f7f33d95e15ee95a96
SijongKim93 <rlatlwhd456@gmail.com>
2024-03-06 21:56:51 +0900

    Lv 2

--- FirstAssignment/main.swift
+++ FirstAssignment/main.swift
@@ -9,7 +9,7 @@
 //
 
 // Lv 1
-class Calculator {
+class Calculator1 {
     func calculate(_ operatorString: String, _ firstNumber: Double, _ secondNumber: Double) -> Double {
         switch operatorString {
         case "+":
@@ -29,7 +29,7 @@ class Calculator {
     }
 }
 
-let calculator = Calculator()
+let calculator = Calculator1()
 
 let result1 = calculator.calculate("+", 10, 5)
 let result2 = calculator.calculate("-", 10, 5)
@@ -42,5 +42,41 @@ for i in result {
     print("값은 \(i)입니다.")
     
 }
+
+
+
+// Lv2
+
+class Calculator2 {
+    func calculate(_ operatorString: String, _ firstNumber: Double, _ secondNumber: Double) -> Double {
+        switch operatorString {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "*":
+            return firstNumber * secondNumber
+        case "/":
+            guard secondNumber != 0 else {
+                return 0
+            }
+            return firstNumber / secondNumber
+        case "%":
+            guard secondNumber != 0 else {
+                return 0
+            }
+            return firstNumber.truncatingRemainder(dividingBy: secondNumber)
+        default:
+            return 0
+        }
+    }
+}
+
+
+let calculator2 = Calculator2()
+
+let result11 = calculator2.calculate("%", 10, 3)
+print(result11)
+
 
 
