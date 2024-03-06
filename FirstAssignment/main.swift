7a21945afa990174c75f9f8136fe22751a26e825
SijongKim93 <rlatlwhd456@gmail.com>
2024-03-06 19:21:21 +0900

    Lv1

--- FirstAssignment/main.swift
+++ FirstAssignment/main.swift
@@ -5,5 +5,25 @@
 //  Created by 김시종 on 3/6/24.
 //
 
-import Foundation
+//import Foundation
+//
+
+// Lv 1
+class Calculator {
+    func calculate(_ operation: String, _ firstNumber: Double, _ secondNumber: Double) -> Double? {
+        switch operation {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "*":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+        default:
+            return 0
+        }
+    }
+}
+
 
