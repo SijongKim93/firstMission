d2cc7c6f18c6bcfb16515684daf4ba51d42b3c3e
SijongKim93 <rlatlwhd456@gmail.com>
2024-03-06 20:29:29 +0900

    Lv1 끝

--- FirstAssignment/main.swift
+++ FirstAssignment/main.swift
@@ -10,8 +10,8 @@
 
 // Lv 1
 class Calculator {
-    func calculate(_ operation: String, _ firstNumber: Double, _ secondNumber: Double) -> Double? {
-        switch operation {
+    func calculate(_ operatorString: String, _ firstNumber: Double, _ secondNumber: Double) -> Double {
+        switch operatorString {
         case "+":
             return firstNumber + secondNumber
         case "-":
@@ -19,11 +19,28 @@ class Calculator {
         case "*":
             return firstNumber * secondNumber
         case "/":
+            guard secondNumber != 0 else {
+                return 0
+            }
             return firstNumber / secondNumber
         default:
             return 0
         }
     }
+}
+
+let calculator = Calculator()
+
+let result1 = calculator.calculate("+", 10, 5)
+let result2 = calculator.calculate("-", 10, 5)
+let result3 = calculator.calculate("*", 10, 5)
+let result4 = calculator.calculate("/", 10, 0)
+
+let result: [Double] = [result1, result2, result3, result4]
+
+for i in result {
+    print("값은 \(i)입니다.")
+    
 }
 
 
